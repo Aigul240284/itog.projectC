628b29c1288c981f3ef70da0241d7cf6d75ae1db
Aigul <gayazovaaigulgazimovna@gmail.com>
2022-11-21 21:46:02 +0300

    DZ_3

---
+++ DZ_3/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29*/
+
+Console.Write("Введите неотрицательнoe число M: ");
+int m = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Введите неотрицательнoe число N: ");
+int n = int.Parse(Console.ReadLine() ?? "");
+
+int FindFunctionAckerman(int m, int  n)
+{
+    if (m==0)
+        return n+1;
+    else
+        if ((m!=0) && (n==0))
+            return FindFunctionAckerman(m-1,1);
+        else
+            return FindFunctionAckerman(m-1,FindFunctionAckerman(m,n-1));
+}
+Console.WriteLine($"A({m},{n}) -> {FindFunctionAckerman(m,n)}");
